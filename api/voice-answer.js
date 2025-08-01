e190a2b82090091aee8a373049172016c046ee47
Sahil Khanna <sahil.khanna@leoforce.com>
2025-08-01 18:01:05 +0530

    vercel file changes

--- api/voice-answer.js
+++ api/voice-answer.js
@@ -2,23 +2,23 @@ export default function handler(req, res) {
     const query = req.query;
     const vonageNumber = "12012750661";
 
-    // if (query.endpoint_type === "app") {
-    //     console.log("NCCO request:");
-    //     console.log(`  - callee: ${query.to}`);
-    //     console.log("---");
+    if (query.endpoint_type === "app") {
+        console.log("NCCO request:");
+        console.log(`  - callee: ${query.to}`);
+        console.log("---");
 
-    //     res.status(200).json([
-    //         {
-    //             action: "talk",
-    //             text: "Please wait while we connect you.",
-    //         },
-    //         {
-    //             action: "connect",
-    //             from: vonageNumber,
-    //             endpoint: [{ type: "phone", number: query.to }],
-    //         },
-    //     ]);
-    // } else {
+        res.status(200).json([
+            {
+                action: "talk",
+                text: "Please wait while we connect you.",
+            },
+            {
+                action: "connect",
+                from: vonageNumber,
+                endpoint: [{ type: "phone", number: query.to }],
+            },
+        ]);
+    } else {
         console.log("NCCO request:");
         console.log(`  - caller: ${query.from}`);
         console.dir(query);
@@ -35,5 +35,5 @@ export default function handler(req, res) {
                 endpoint: [{ type: "app", user: "alice" }],
             },
         ]);
-    // }
+    }
 }
